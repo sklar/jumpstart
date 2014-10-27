cf899180997ed6f14b671874eb50abead85e1815
David Sklář <sklar@avast.com>
2014-10-27 15:57:43 +0100

    Add animations.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -11,7 +11,9 @@
 	 */
 	var WF = WF || {};
 
-
+	/**
+	 *
+	 */
 	WF.features = (function () {
 		var vh = $(window).height(),
 			className = 'is-animated';
@@ -25,8 +27,61 @@
 					$this.addClass(className);
 				}
 			},
-			{ offset: vh + $(this).height() }
+			{
+				offset: function () {
+					return vh + $(this).height();
+				}
+			}
 		);
+	})();
+
+	/**
+	 *
+	 */
+	WF.head = (function () {
+		var target = document.querySelector('.page__head'),
+			targetBounding = target.getBoundingClientRect(),
+			targetPositionTopLimit = targetBounding.height + 50,
+			targetPlaceholder = document.createElement('div'),
+			activeClassName = 'is-sticky',
+			state = false,
+			isTargetPlaceholderSet = false,
+			isTouch = ('ontouchstart' in window) ? true : false;
+
+		targetPlaceholder.style.width = targetBounding.width + 'px';
+		targetPlaceholder.style.height = targetBounding.height + 'px';
+
+		/**
+		 * Add `scroll` event listener.
+		 */
+		window.addEventListener('scroll', function() {
+			if (!state || isTouch) {
+				state = true;
+				return window.setTimeout(function () {
+					state = false;
+					toggle();
+				}, 0);
+			}
+		});
+
+		/**
+		 * Toggles element `sticky` className and placeholder.
+		 */
+		function toggle() {
+			var pageYOffset = window.pageYOffset,
+				targetPositionTop = targetBounding.top + targetPositionTopLimit;
+
+			if (pageYOffset >= targetPositionTop && !isTargetPlaceholderSet) {
+				target.classList.add(activeClassName);
+				target.parentNode.insertBefore(targetPlaceholder, target);
+				isTargetPlaceholderSet = true;
+			} else if (pageYOffset < targetPositionTop && isTargetPlaceholderSet) {
+				target.classList.remove(activeClassName);
+				target.parentNode.removeChild(targetPlaceholder);
+				isTargetPlaceholderSet = false;
+			}
+		}
+
 	})();
 
 
